1b3adc44cc929962b7b55ff4ba4faea6986dbe1e
freemanmaxwell <86249555+freemanmaxwell@users.noreply.github.com>
2023-06-15 09:55:21 -0500

    changed icons
    changed fontawesome icons

--- projects/script.js
+++ projects/script.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Alfaizkhan Pathan";
+            document.title = "Maxwell Freeman";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Maxwell Freeman";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
